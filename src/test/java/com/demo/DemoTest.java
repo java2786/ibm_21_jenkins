95d41c8368d414dd43f206ec22b0e26eb5848e50
Arun Kumar <java.kumar.arun@gmail.com>
2021-03-12 00:36:19 +0530

    jenkins + maven

---
+++ src/test/java/com/demo/DemoTest.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DemoTest {
+
+    @Test
+    public void testMethod_1(){
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void testMethod_2(){
+        Assert.assertFalse(true);
+    }
+    
+}
